a46f71ec2e911216e72260623a6dd3968984b45a
tkrkt <tkrkt2773@gmail.com>
2019-09-10 22:57:29 +0900

    change looks-same format to pass tests

--- spec/text2pngSpec.js
+++ spec/text2pngSpec.js
@@ -9,26 +9,33 @@ const text2png = require('../index.js');
 const looksSame = require('looks-same');
 
 describe('text2png', () => {
-  glob.sync(path.resolve(__dirname, 'testcases', '*.json')).forEach(filePath => {
-    const fileName = path.basename(filePath, '.json');
-    console.log(fileName);
+  glob
+    .sync(path.resolve(__dirname, 'testcases', '*.json'))
+    .forEach(filePath => {
+      const fileName = path.basename(filePath, '.json');
+      console.log(fileName);
 
-    it('matches ' + fileName, () => {
-      const config = JSON.parse(fs.readFileSync(filePath));
+      it('matches ' + fileName, () => {
+        const config = JSON.parse(fs.readFileSync(filePath));
 
-      return new Promise((resolve, reject) => {
-        looksSame(
-          text2png.apply(text2png, config),
-          fs.readFileSync(path.join(__dirname, 'expected', fileName + '.png')),
-          { tolerance: 0.2, ignoreAntialiasing: true },
-          (error, match) => {
-            if (error) reject(error);
-
-            expect(match).toBe(true, 'generated image does not match');
-            resolve();
-          }
-        );
+        return new Promise((resolve, reject) => {
+          looksSame(
+            text2png.apply(text2png, config),
+            fs.readFileSync(
+              path.join(__dirname, 'expected', fileName + '.png')
+            ),
+            {
+              tolerance: 0.2,
+              ignoreAntialiasing: true,
+              antialiasingTolerance: 3
+            },
+            (error, { equal }) => {
+              if (error) reject(error);
+              expect(equal).toBe(true, 'generated image does not match');
+              resolve();
+            }
+          );
+        });
       });
     });
-  });
 });
